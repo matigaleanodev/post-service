c877100240b0bb3885ca7815b416d929a8716de6
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2025-03-15 17:44:41 -0300

    feat: creación de schema de Post e instalación de MongoDB

--- src/models/post.interface.ts
+++ src/models/post.interface.ts
@@ -1,3 +1,24 @@
-export interface Post {
-  id: number;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type PostDocument = HydratedDocument<Post>;
+
+@Schema({ timestamps: true })
+export class Post {
+  @Prop({ type: String })
+  _id?: string;
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ default: [] })
+  tags?: string[];
+
+  @Prop({ default: false })
+  published?: boolean;
 }
+
+export const PostSchema = SchemaFactory.createForClass(Post);
