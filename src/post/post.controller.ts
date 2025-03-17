3c46a226d5fff4420ab9a600aca10bea0706c4cf
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2025-03-17 19:14:14 -0300

    feat: conexión entre API Gateway y microservicio Post Service

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,34 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
-@Controller('post')
+@Controller()
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
-  @Post()
-  create(@Body() createPostDto: CreatePostDto) {
+  @MessagePattern('createPost')
+  create(@Payload() createPostDto: CreatePostDto) {
     return this.postService.create(createPostDto);
   }
 
-  @Get()
+  @MessagePattern('findAllPost')
   findAll() {
     return this.postService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postService.findOne(+id);
+  @MessagePattern('findOnePost')
+  findOne(@Payload() id: string) {
+    return this.postService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postService.update(+id, updatePostDto);
+  @MessagePattern('updatePost')
+  update(
+    @Payload()
+    { id, updatePostDto }: { id: string; updatePostDto: UpdatePostDto },
+  ) {
+    return this.postService.update(id, updatePostDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.postService.remove(+id);
+  @MessagePattern('removePost')
+  remove(@Payload() id: string) {
+    return this.postService.remove(id);
   }
 }
